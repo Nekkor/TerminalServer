b0e5a1043715425e3e4ce45ebf261fc8e4affd71
Nekkor <bga@nxt.ru>
2019-06-01 00:05:19 +0300

    Get User CPU & Memory

--- CheckUsageREsources.ps1
+++ CheckUsageREsources.ps1
@@ -13,3 +13,36 @@
 .NOTES
     General notes
 #>
+
+#Get Logon Users
+Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+$Users = [System.DirectoryServices.AccountManagement.UserPrincipal]::Current | Select-Object Name,SID
+
+$Summary = New-Object System.Collections.ArrayList
+
+#collect data
+foreach ($user in $users){
+    $Processes = Get-Process -IncludeUserName | Where-Object -Property UserName -like $user.UserName
+    $USerMB =  ($Processes| Measure-Object WorkingSet -sum).Sum / 1kb
+    $USerCPU = ($Processes | Measure-Object CPU -sum).Sum
+    
+    $item = New-Object psobject
+    $Item | Add-Member NoteProperty userName    $user.Name
+    $Item | Add-Member NoteProperty userSid     $user.Sid
+    $Item | Add-Member NoteProperty CPUTotal    $USerCPU
+    $Item | Add-Member NoteProperty MBTotal     $USerMB
+    $Summary.Add($item) |Out-Null
+}
+$Summary
+
+
+
+
+
+
+
+([wmi]"").ConvertToDateTime((Get-WmiObject -Class Win32_UserProfile | Where-Object SID -eq $user.SID).LastUseTime)
+
+
+$wmi = Get-WmiObject -Class Win32_UserProfile | Where-Object SID -eq $user.SID
+$wmi
